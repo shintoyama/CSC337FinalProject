2bf8ca11d7f438a524231d8759ab195cdbbae09d
stoyama <socrakun@gmail.com>
2018-11-25 14:47:17 -0700

    functions added

---
+++ controller.php
@@ -0,0 +1,26 @@
+<?php 
+//CSC337 Final Project
+//Ben Lazarines
+//Shin Toyama
+//12/05/2018
+
+include "model.php";
+$theDBA = new DataBaseAdaptor ();
+
+if (isset($_GET['WOList'])) {
+
+}
+if (isset($_GET['WOHistory'])) {
+	
+}
+if (isset($_GET['signInUN'] && $_GET['signInPW'])) {
+	
+}
+if (isset($_GET['signUpUN'] && $_GET['signUpPW'])) {
+	
+}
+if (isset($_GET[''])) {
+	
+}
+
+?>
---
+++ model.php
@@ -0,0 +1,16 @@
+<?php 
+//CSC337 Final Project
+//Ben Lazarines
+//Shin Toyama
+//12/05/2018
+
+class DataBaseAdaptor {
+    private $DB;
+
+    public function __construct() {
+
+    }
+
+}
+
+?>
