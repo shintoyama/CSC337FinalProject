b412c7c4fcd7d45d30097ebccab05751dcf91b83
benlazarine <benlazarine@email.arizona.edu>
2018-12-05 07:18:59 -0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //CSC337 Final Project
 //Ben Lazarines
 //Shin Toyama
@@ -11,16 +11,34 @@ if (isset($_GET['WOList'])) {
 
 }
 if (isset($_GET['WOHistory'])) {
-	
+
 }
 if (isset($_GET['signInUN'] && $_GET['signInPW'])) {
-	
+
 }
 if (isset($_GET['signUpUN'] && $_GET['signUpPW'])) {
-	
+	$user = $_GET['signUpUN'];
+	$pass = $_GET['signUpPW'];
+  $arr = $theDBA->checkUser ( $user );
+	$used = 1;
+
+	foreach ($arr as $value){
+		if ($user == $value){
+			$used = 0;
+			alert ($user . "is already being used, please use a different username");
+		}
+	}
+	if ($used == 1){
+		$arr = $theDBA->submitUser ( $user, $pass );
+		echo($arr);
+	}
+
+//  echo json_encode( $arr );
+
+
 }
 if (isset($_GET[''])) {
-	
+
 }
 
 ?>
