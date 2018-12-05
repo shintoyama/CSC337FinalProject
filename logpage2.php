00f3f2c89dd6cffb546ac5a8923c38bec4a9b4fa
stoyama <socrakun@gmail.com>
2018-12-05 12:31:10 -0700

    log page updated

--- logpage2.php
+++ logpage2.php
@@ -28,7 +28,7 @@ session_start();
 		<th width="10%">Log</th>
 	</tr>
 	<tr>
-	<form onsubmit="addWorkoutL(2)">
+	<form onsubmit="addWorkoutL(2);return false">
 		<td>Benchpress</td>
 		<td>10</td>
 		<td><input type="number" id="WOLweight2" placeholder="weights" min="0" max="1000000" /></td>
@@ -37,7 +37,7 @@ session_start();
 	</form>
 	</tr>
 	<tr>
-	<form onsubmit="addWorkoutL(3)">
+	<form onsubmit="addWorkoutL(3);return false">
 		<td>Push Up</td>
 		<td>10</td>
 		<td><input type="number" id="WOLweight3" placeholder="weights" min="0" max="1000000" /></td>
@@ -45,7 +45,7 @@ session_start();
 		<td width="10%"><input type="submit" class="nbutton" value="Log"/></td>
 	</tr>
 	<tr>
-	<form onsubmit="addWorkoutL(4)">
+	<form onsubmit="addWorkoutL(4);return false">
 		<td>Shoulder Press</td>
 		<td>10</td>
 		<td><input type="number" id="WOLweight4" placeholder="weights" min="0" max="1000000" /></td>
@@ -54,7 +54,7 @@ session_start();
 	</form>
 	</tr>
 	<tr>
-	<form onsubmit="addWorkoutL(6)">
+	<form onsubmit="addWorkoutL(6);return false">
 		<td>Curls</td>
 		<td>10</td>
 		<td><input type="number" id="WOLweight6" placeholder="weights" min="0" max="1000000" /></td>
@@ -63,7 +63,7 @@ session_start();
 	</form>
 	</tr>
 	<tr>
-	<form onsubmit="addWorkoutL(7)">
+	<form onsubmit="addWorkoutL(7);return false">
 		<td>Pull Up</td>
 		<td>10</td>
 		<td><input type="number" id="WOLweight7" placeholder="weights" min="0" max="1000000" /></td>
@@ -72,7 +72,7 @@ session_start();
 	</form>
 	</tr>
 	<tr>
-	<form onsubmit="addWorkoutL(5)">
+	<form onsubmit="addWorkoutL(5);return false">
 		<td>Crunch</td>
 		<td>10</td>
 		<td><input type="number" id="WOLweight5" placeholder="weights" min="0" max="1000000" /></td>
@@ -86,12 +86,12 @@ session_start();
 </center>
 
 <script type="text/javascript">
-	function addWorkout(n) {
+	function addWorkoutL(n) {
 		var idSets = 'WOLsets' + n;
 		var idWeight = 'WOLweight' + n;
 		var nofsets = document.getElementById(idSets);
 		var weight = document.getElementById(idWeight);
-		ajax.open("GET", "controller.php?quickWO=" + n + "&sets=" + nofsets.value + "&weight=" + weight.value, true);
+		ajax.open("GET", "controller.php?WOLlog=" + n + "&sets=" + nofsets.value + "&weight=" + weight.value, true);
 		ajax.send();
 		ajax.onreadystatechange = function () {
 	        if (ajax.readyState == 4 && ajax.status == 200) {
