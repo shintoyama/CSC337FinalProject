702295b26d12b25fc3737ddca33bdbf448bdbfcf
benlazarine <benlazarine@email.arizona.edu>
2018-12-05 10:47:32 -0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -6,6 +6,7 @@
 
 include "model.php";
 $theDBA = new DataBaseAdaptor ();
+$currentUser = "";
 
 if (isset($_GET['WOList'])) {
 
@@ -15,22 +16,31 @@ if (isset($_GET['WOHistory'])) {
 }
 
 if (isset($_GET['quickWO'])) {
-	$workout = $_GET['quickWO'];
-	$sets = $_GET['sets'];
-// FIGURE OUT HOW TO PULL USER_ID
-	if ($workout == 1){
-		$theDBA->logCardio($user, $workout, $sets);
+	if ($currentUser == ""){
+		echo($currentUser . "You must be logged in to perform this action");
 	}
-// ADD WEIGHTS FOR LIFT
-	else {
-		$theDBA->logLift($user, $workout, $weight, $sets);
+	else{
+		$workout = $_GET['quickWO'];
+		$sets = $_GET['sets'];
+	// FIGURE OUT HOW TO PULL USER_ID
+		if ($workout == 1){
+			$theDBA->logCardio($currentUser, $workout, $sets);
+			echo("success");
+		}
+	// ADD WEIGHTS FOR LIFT
+		else {
+			$weight = $_GET['weight'];
+			$theDBA->logLift($currentUser, $workout, $weight, $sets);
+		}
 	}
+
 }
 
 if (isset($_GET['signInUN']) && isset($_GET['signInPW'])) {
 	$user = $_GET['signInUN'];
 	$pass = $_GET['signInPW'];
   $arr = $theDBA->signIn ( $user, $pass );
+	$currentUser = $user;
 	echo($arr);
 
 }
@@ -40,12 +50,12 @@ if (isset($_GET['signUpUN']) && isset($_GET['signUpPW'])) {
   $arr = $theDBA->checkUser ( $user );
 
 		if ($arr != null){
-			echo ("'" . $user . "' is already being used, please use a different username");
+			echo ("fail");
 		}
 
 	else{
 		$arr = $theDBA->submitUser ( $user, $pass );
-		echo($arr);
+		echo("success");
 	}
 
 //  echo json_encode( $arr );
