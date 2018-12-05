69cc4845121641538f17242959b98abd73681be0
benlazarine <benlazarine@email.arizona.edu>
2018-12-05 08:00:19 -0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -13,22 +13,22 @@ if (isset($_GET['WOList'])) {
 if (isset($_GET['WOHistory'])) {
 
 }
-if (isset($_GET['signInUN'] && $_GET['signInPW'])) {
+if (isset($_GET['signInUN']) && isset($_GET['signInPW'])) {
+	$user = $_GET['signInUN'];
+	$pass = $_GET['signInPW'];
+  $arr = $theDBA->signIn ( $user, $pass );
 
 }
-if (isset($_GET['signUpUN'] && $_GET['signUpPW'])) {
+if (isset($_GET['signUpUN']) && isset($_GET['signUpPW'])) {
 	$user = $_GET['signUpUN'];
 	$pass = $_GET['signUpPW'];
   $arr = $theDBA->checkUser ( $user );
-	$used = 1;
 
-	foreach ($arr as $value){
-		if ($user == $value){
-			$used = 0;
-			alert ($user . "is already being used, please use a different username");
+		if ($arr != null){
+			echo ("'" . $user . "' is already being used, please use a different username");
 		}
-	}
-	if ($used == 1){
+
+	else{
 		$arr = $theDBA->submitUser ( $user, $pass );
 		echo($arr);
 	}
