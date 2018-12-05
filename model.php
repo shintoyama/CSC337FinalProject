dbe46e9d8cf8fb37ee671b4b346f2b774974ee70
benlazarine <benlazarine@email.arizona.edu>
2018-12-05 11:58:00 -0700

    Update model.php

--- model.php
+++ model.php
@@ -65,17 +65,29 @@ class DataBaseAdaptor {
   }
 
   public function logCardio($user, $workout, $sets){
-    $stmt = $this->DB->prepare ( "INSERT into cardio_log (user_id, cardio_id, workout_count) VALUES (:user, :cardio, :count);" );
+    $stmt = $this->DB->prepare ( "INSERT into cardio_log (username, cardio_name, workout_count) VALUES (:user, :cardio, :count);" );
     $stmt->bindParam ('user', $user);
-    $stmt->bindParam ('cardio', $workout);
+    $stmt->bindParam ('cardio', "run");
     $stmt->bindParam ('count', $sets);
     $stmt->execute ();
   }
 
   public function logLift($user, $workout, $weight, $sets){
-    $stmt = $this->DB->prepare ( "INSERT into lifting_log (user_id, lifting_id, weight, sets) VALUES (:user, :lift, :weight, :count);" );
+    $muscle = "";
+    if ($workout == 2) $workout = "Bench Press";
+    else if ($workout == 3) $workout = "Push Ups";
+    else if ($workout == 4) $workout = "Shoulder Press";
+    else if ($workout == 5) $workout = "Crunches";
+
+    if ($workout == "Bench Press" || $workout == "Push Ups") $muscle = "Chest";
+    else if ($workout == "Shoulder Press") $muscle = "Shoulders";
+    else if ($workout == "Curls" || $workout == "Pull Ups") $muscle = "Arms";
+    else $muscle = "Core";
+
+    $stmt = $this->DB->prepare ( "INSERT into lifting_log (username, lifting_name, muscle_group, weight, sets) VALUES (:user, :lift, :muscle, :weight, :count);" );
     $stmt->bindParam ('user', $user);
     $stmt->bindParam ('lift', $workout);
+    $stmt->bindParam ('muscle', $muscle);
     $stmt->bindParam ('weight', $weight);
     $stmt->bindParam ('count', $sets);
     $stmt->execute ();
